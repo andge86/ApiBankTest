5bad68a5520c595d80559400fce57cfa163918a8
andge86 <andre11241986@gmail.com>
2018-11-18 22:52:01 +0100

    Add files via upload

---
+++ src/main/java/Gold.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Gold {
+
+    public List<GoldRates> getGoldRates() {
+        return CenaZlota;
+    }
+
+    List<GoldRates> CenaZlota = new ArrayList();
+
+}
---
+++ src/main/java/GoldRates.java
@@ -0,0 +1,17 @@
+public class GoldRates {
+
+
+  private  String data;
+  private  float cena;
+
+    public String getData() {
+        return data;
+    }
+
+    public float getCena() {
+        return cena;
+    }
+
+
+
+}
